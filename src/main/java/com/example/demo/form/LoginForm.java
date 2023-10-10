579f0f1179eb911036a4d0978dcce7435c046524
RaggedCloth <hukurahagix@yahoo.co.jp>
2023-10-10 13:03:11 +0900

    entity

--- src/main/java/com/example/demo/form/LoginForm.java
+++ src/main/java/com/example/demo/form/LoginForm.java
@@ -2,10 +2,18 @@ package com.example.demo.form;
 
 import lombok.Data;
 
+/**
+ * ログイン画面 form
+ * 
+ * @author ys-fj
+ *
+ */
 @Data
 public class LoginForm {
 
+	/** ログインID */
 	private String loginId;
-	
+
+	/** パスワード */
 	private String password;
 }
