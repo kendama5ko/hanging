a7b5247a2a56f1f2426a5a527285be1fa49deffd
raggedcloth <hukurahagix@yahoo.co.jp>
2023-10-16 23:52:10 +0900

    ABC 085 C - Otoshidama

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,32 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-    List<Integer> a = new ArrayList<>();
-    for (int i = 0; i < N; i++) {
-      a.add(sc.nextInt());
-    }
+    int Y = sc.nextInt();
+    var res = false;
+    int z = 0;
     sc.close();
-    Collections.sort(a);
-    Collections.reverse(a);
-    int alice = 0;
-    int bob = 0;
-    int res = 0;
-    for (int i = 0; i < N; i++) {
-      if ( i % 2 == 1) {
-        bob += a.get(i);
-      } else {
-        alice += a.get(i);
+    label: for (int i = 0; i <= N; i++) {
+      if (Y == 10000 * i && i == N) {
+        System.out.println(i + " " + 0 + " " + 0);
+        res = true;
+        break label;
+      }
+      for (int j = 0; j <= N; j++) {
+        if (Y == 10000 * i + 5000 * j && i + j == N) {
+          System.out.println(i + " " + j + " " + 0);
+          res = true;
+          break label;
+        } else if ((Y - (10000 * i + 5000 * j)) % 1000 == 0) {
+          z = (Y - (10000 * i + 5000 * j)) / 1000;
+          if (z + i + j == N && z > 0) {
+            System.out.println(i + " " + j + " " + z);
+            res = true;
+            break label;
+          }
+        }
       }
     }
-    res = alice - bob;
-    
-    System.out.println(alice - bob);
-
-    
+    if (!res)
+      System.out.println(-1 + " " + -1 + " " + -1);
   }
 }
