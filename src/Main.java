ffe975292bc4e73c81bfb4cc34c3f1b129779d9a
raggedcloth <hukurahagix@yahoo.co.jp>
2023-10-19 22:05:31 +0900

    ABC 086 C - Traveling

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,38 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String S = sc.nextLine();
-    String key1 = "dreamer";
-    String key2 = "eraser";
-    String key3 = "dream";
-    String key4 = "erase";
+    int N = sc.nextInt();
+    int[] t = new int[N+1];
+    int[] x = new int[N+1];
+    int[] y = new int[N+1];
+    t[0] = 0;
+    x[0] = 0;
+    y[0] = 0;
+    for (int i = 1; i <= N; i++) {
+      t[i] = sc.nextInt();
+      x[i] = sc.nextInt();
+      y[i] = sc.nextInt();
+    }
     sc.close();
-
-    while (!S.isEmpty())
-      if (S.endsWith(key1)) {
-        S = S.substring(0, S.length() - key1.length());
-      } else if (S.endsWith(key2)) {
-        S = S.substring(0, S.length() - key2.length());
-      } else if (S.endsWith(key3)) {
-        S = S.substring(0, S.length() - key3.length());
-      } else if (S.endsWith(key4)) {
-        S = S.substring(0, S.length() - key4.length());
-      } else {
+    var res = false;
+    for (int i = 0; i < N; i++) {
+      int turn = 0;
+      int distance = 0;
+      if (i == N) {
         break;
       }
-    if (S.length() != 0) {
-      System.out.println("NO");
-    } else {
-      System.out.println("YES");
+      turn = t[i + 1] - t[i];
+      distance = Math.abs((x[i] + y[i]) - (x[i + 1] + y[i + 1]));
+      if (turn - distance == 1 || turn % 2 != distance % 2 
+          || turn - distance < 0) {
+        System.out.println("No");
+        res = false;
+        break;
+      } else {
+        res = true;
+      }
     }
+    if (res)
+      System.out.println("Yes");
   }
 }
